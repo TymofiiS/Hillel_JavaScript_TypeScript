c241aef08c0e676d5e8ccd150885a0eb54b47666
Tymofii Serhiienko <tymofii.serhiienko@gmail.com>
2023-10-22 14:23:57 +0300

    continue learning

--- TypeAssertionAndGuards/type_assertion.ts
+++ TypeAssertionAndGuards/type_assertion.ts
@@ -16,3 +16,67 @@ let fish2 = nemo as Fish1;
 let fish3 = <Fish1>(<any>notNemo);
 
 //********************************* */
+class DataProvider {
+  constructor(public data: any) {}
+}
+
+let str = new DataProvider("string");
+let arr: string[] = (str.data as string).split(",");
+
+//*********************************** */
+type StatusCode = 200 | 404;
+type RequestBody = {
+  status: StatusCode;
+};
+let statusCode = 200 as const;
+const requestBody: RequestBody = { status: statusCode };
+
+//******************************** */
+type NotConstType = {
+  status: number;
+  data: {
+    role: string;
+  };
+};
+
+type ConstType = {
+  status: 200 | 404;
+  data: {
+    role: "user" | "admin";
+  };
+};
+
+let a: ConstType = {
+  status: 200,
+  data: {
+    role: "user",
+  },
+} as const;
+
+a.status = 404;
+a.data.role = "user";
+
+//******************************* */
+function isStringAssert(condition: any): asserts condition {
+  if (!condition) {
+    throw new Error("");
+  }
+}
+
+function isString(value: any): value is string {
+  return typeof value === "string";
+}
+
+function isStringAssertCombined(value: any): asserts value is string {
+  if (typeof value !== "string") {
+    throw new Error("");
+  }
+}
+
+const testScope = (text: any) => {
+  try {
+    //isStringAssert(isString(text));
+    isStringAssertCombined(text);
+    text.toUpperCase();
+  } catch (error) {}
+};
