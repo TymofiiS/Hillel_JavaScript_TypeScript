ca085b5fc412c332cfed9a43e592d8ce95acda50
Tymofii Serhiienko <tymofii.serhiienko@gmail.com>
2023-11-20 15:16:10 +0200

    small fix

--- hw8/hw8.ts
+++ hw8/hw8.ts
@@ -9,11 +9,7 @@ todoList.addNote(new Note("name1", "description1"));
 todoList.addNote(new Note("name7", "description7"));
 
 console.log(JSON.stringify(todoList.getAllNotes()));
-const n = new Note("name9", "description9");
-console.log(JSON.stringify(n.clone()));
-//console.log(todoList.getNotes().filter((x) => x.getName() === "name5")[0]);
 
-/*
 todoList.getNoteByName("name1")?.setStatus(Status.Done);
 todoList.setNoteDone(todoList.getNoteByName("name5"));
 
@@ -23,4 +19,3 @@ todoList.updateNote(note);
 
 console.log(JSON.stringify(todoList.notesSortedByStatus()));
 console.log(JSON.stringify(todoList.getActiveNoteCount()));
-*/
